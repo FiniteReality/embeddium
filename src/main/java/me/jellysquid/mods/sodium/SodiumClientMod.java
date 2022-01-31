bf567de73f20eaa3f57b06dce670e2a78de30f31
Asek3 <bogban.zp@gmail.com>
2022-01-31 15:12:22 +0200

    Bump version

--- src/main/java/me/jellysquid/mods/sodium/SodiumClientMod.java
+++ src/main/java/me/jellysquid/mods/sodium/SodiumClientMod.java
@@ -15,7 +15,7 @@ public class SodiumClientMod {
     private static UserConfig CONFIG;
     private static Logger LOGGER;
 
-    private static String MOD_VERSION = "0.4.0";
+    private static String MOD_VERSION = "0.4.0a";
     
     public static final String MODID = "rubidium";
 
