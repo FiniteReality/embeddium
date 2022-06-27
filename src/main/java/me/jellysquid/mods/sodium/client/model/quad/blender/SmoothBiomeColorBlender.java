cb53898fd4dfd58dc0121592be3cf3d9910cf2c8
Nano <bogban.zp@gmail.com>
2022-06-27 11:44:18 +0300

    Revert BiomeBlend changes
    to be compatible with cavebiomeapi

--- src/main/java/me/jellysquid/mods/sodium/client/model/quad/blender/SmoothBiomeColorBlender.java
+++ src/main/java/me/jellysquid/mods/sodium/client/model/quad/blender/SmoothBiomeColorBlender.java
@@ -1,12 +1,13 @@
 package me.jellysquid.mods.sodium.client.model.quad.blender;
 
 import me.jellysquid.mods.sodium.client.model.quad.ModelQuadView;
+import me.jellysquid.mods.sodium.client.model.quad.properties.ModelQuadFlags;
+import me.jellysquid.mods.sodium.client.util.color.ColorABGR;
 import me.jellysquid.mods.sodium.client.util.color.ColorARGB;
-import me.jellysquid.mods.sodium.client.util.color.ColorMixer;
+import me.jellysquid.mods.sodium.client.util.color.ColorU8;
 import net.minecraft.block.BlockState;
 import net.minecraft.client.color.block.BlockColorProvider;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.BlockRenderView;
 
 public class SmoothBiomeColorBlender implements BiomeColorBlender {
@@ -16,77 +17,101 @@ public class SmoothBiomeColorBlender implements BiomeColorBlender {
 
     @Override
     public int[] getColors(BlockColorProvider colorizer, BlockRenderView world, BlockState state, BlockPos origin,
-            ModelQuadView quad) {
+                           ModelQuadView quad) {
         final int[] colors = this.cachedRet;
 
+        boolean aligned = ModelQuadFlags.contains(quad.getFlags(), ModelQuadFlags.IS_ALIGNED);
+
         for (int i = 0; i < 4; i++) {
-        	colors[i] = this.getInterpolatedVertexColor(colorizer, world, state, origin, quad, i);
+            // If the vertex is aligned to the block grid, we do not need to interpolate
+            if (aligned) {
+                colors[i] = this.getVertexColor(colorizer, world, state, origin, quad, i);
+            } else {
+                colors[i] = this.getInterpolatedVertexColor(colorizer, world, state, origin, quad, i);
+            }
         }
 
         return colors;
     }
 
-    private int getBlockColor(BlockColorProvider colorizer, BlockRenderView world, BlockState state,
-            int x, int y, int z, int colorIdx) {
-        return colorizer.getColor(state, world, this.mpos.set(x, y, z), colorIdx);
+    private int getVertexColor(BlockColorProvider colorizer, BlockRenderView world, BlockState state, BlockPos origin,
+                               ModelQuadView quad, int vertexIdx) {
+        final int x = origin.getX() + (int) quad.getX(vertexIdx);
+        final int z = origin.getZ() + (int) quad.getZ(vertexIdx);
+
+        final int color = this.getBlockColor(colorizer, world, state, origin, x, z, quad.getColorIndex());
+
+        return ColorARGB.toABGR(color);
+    }
+
+    private int getBlockColor(BlockColorProvider colorizer, BlockRenderView world, BlockState state, BlockPos origin,
+                              int x, int z, int colorIdx) {
+        return colorizer.getColor(state, world, this.mpos.set(x, origin.getY(), z), colorIdx);
     }
 
     private int getInterpolatedVertexColor(BlockColorProvider colorizer, BlockRenderView world, BlockState state,
-            BlockPos origin, ModelQuadView quad, int vertexIdx) {
-    	// Clamp positions to the range -1.0f to +2.0f to prevent crashes with badly behaved
-        // block models and mods causing out-of-bounds array accesses in BiomeColorCache.
-        // Offset the position by -0.5f after clamping to align smooth blending with flat blending.
-        final float x = MathHelper.clamp(quad.getX(vertexIdx), -1.0f, 2.0f) - 0.5f;
-        final float y = MathHelper.clamp(quad.getY(vertexIdx), -1.0f, 2.0f) - 0.5f;
-        final float z = MathHelper.clamp(quad.getZ(vertexIdx), -1.0f, 2.0f) - 0.5f;
+                                           BlockPos origin, ModelQuadView quad, int vertexIdx) {
+        final float x = quad.getX(vertexIdx);
+        final float z = quad.getZ(vertexIdx);
 
-        // Floor the positions here to always get the largest integer below the input
-        // as negative values by default round toward zero when casting to an integer.
-        // Which would cause negative ratios to be calculated in the interpolation later on.
-        final int intX = (int) Math.floor(x);
-        final int intY = (int) Math.floor(y);
-        final int intZ = (int) Math.floor(z);
+        final int intX = (int) x;
+        final int intZ = (int) z;
 
         // Integer component of position vector
         final int originX = origin.getX() + intX;
-        final int originY = origin.getY() + intY;
         final int originZ = origin.getZ() + intZ;
-
-        // Retrieve the color values for each neighbor
-        final int c000 = this.getBlockColor(colorizer, world, state, originX, originY, originZ, quad.getColorIndex());
-        final int c001 = this.getBlockColor(colorizer, world, state, originX, originY, originZ + 1, quad.getColorIndex());
-        final int c100 = this.getBlockColor(colorizer, world, state, originX + 1, originY, originZ, quad.getColorIndex());
-        final int c101 = this.getBlockColor(colorizer, world, state, originX + 1, originY, originZ + 1, quad.getColorIndex());
-
-        final int c010 = this.getBlockColor(colorizer, world, state, originX, originY + 1, originZ, quad.getColorIndex());
-        final int c011 = this.getBlockColor(colorizer, world, state, originX, originY + 1, originZ + 1, quad.getColorIndex());
-        final int c110 = this.getBlockColor(colorizer, world, state, originX + 1, originY + 1, originZ, quad.getColorIndex());
-        final int c111 = this.getBlockColor(colorizer, world, state, originX + 1, originY + 1, originZ + 1, quad.getColorIndex());
 
         // Fraction component of position vector
         final float fracX = x - intX;
-        final float fracY = y - intY;
         final float fracZ = z - intZ;
 
-        int dx1 = ColorMixer.getStartRatio(fracX);
-        int dx2 = ColorMixer.getEndRatio(fracX);
+        // Retrieve the color values for each neighbor
+        final int c1 = this.getBlockColor(colorizer, world, state, origin, originX, originZ, quad.getColorIndex());
+        final int c2 = this.getBlockColor(colorizer, world, state, origin, originX, originZ + 1, quad.getColorIndex());
+        final int c3 = this.getBlockColor(colorizer, world, state, origin, originX + 1, originZ, quad.getColorIndex());
+        final int c4 = this.getBlockColor(colorizer, world, state, origin, originX + 1, originZ + 1, quad.getColorIndex());
 
-        int dy1 = ColorMixer.getStartRatio(fracY);
-        int dy2 = ColorMixer.getEndRatio(fracY);
+        final float fr, fg, fb;
 
-        int dz1 = ColorMixer.getStartRatio(fracZ);
-        int dz2 = ColorMixer.getEndRatio(fracZ);
+        // All the colors are the same, so the results of interpolation will be useless.
+        if (c1 == c2 && c2 == c3 && c3 == c4) {
+            fr = ColorARGB.unpackRed(c1);
+            fg = ColorARGB.unpackGreen(c1);
+            fb = ColorARGB.unpackBlue(c1);
+        } else {
+            // TODO: avoid float conversions here
+            // RGB components for each corner's color
+            final float c1r = ColorARGB.unpackRed(c1);
+            final float c1g = ColorARGB.unpackGreen(c1);
+            final float c1b = ColorARGB.unpackBlue(c1);
 
-        int c00 = ColorMixer.mixARGB(c000, c001, dz1, dz2);
-        int c01 = ColorMixer.mixARGB(c100, c101, dz1, dz2);
-        int c10 = ColorMixer.mixARGB(c010, c011, dz1, dz2);
-        int c11 = ColorMixer.mixARGB(c110, c111, dz1, dz2);
+            final float c2r = ColorARGB.unpackRed(c2);
+            final float c2g = ColorARGB.unpackGreen(c2);
+            final float c2b = ColorARGB.unpackBlue(c2);
 
-        int c0 = ColorMixer.mixARGB(c00, c01, dy1, dy2);
-        int c1 = ColorMixer.mixARGB(c10, c11, dy1, dy2);
+            final float c3r = ColorARGB.unpackRed(c3);
+            final float c3g = ColorARGB.unpackGreen(c3);
+            final float c3b = ColorARGB.unpackBlue(c3);
 
-        int c = ColorMixer.mixARGB(c0, c1, dx1, dx2);
+            final float c4r = ColorARGB.unpackRed(c4);
+            final float c4g = ColorARGB.unpackGreen(c4);
+            final float c4b = ColorARGB.unpackBlue(c4);
 
-        return ColorARGB.toABGR(c);
+            // Compute the final color values across the Z axis
+            final float r1r = c1r + ((c2r - c1r) * fracZ);
+            final float r1g = c1g + ((c2g - c1g) * fracZ);
+            final float r1b = c1b + ((c2b - c1b) * fracZ);
+
+            final float r2r = c3r + ((c4r - c3r) * fracZ);
+            final float r2g = c3g + ((c4g - c3g) * fracZ);
+            final float r2b = c3b + ((c4b - c3b) * fracZ);
+
+            // Compute the final color values across the X axis
+            fr = r1r + ((r2r - r1r) * fracX);
+            fg = r1g + ((r2g - r1g) * fracX);
+            fb = r1b + ((r2b - r1b) * fracX);
+        }
+
+        return ColorABGR.pack(ColorU8.normalize(fr), ColorU8.normalize(fg), ColorU8.normalize(fb));
     }
 }
