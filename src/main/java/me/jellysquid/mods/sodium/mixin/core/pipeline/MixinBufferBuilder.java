871e9e6dc200e08676dd1a7bd074fd48ad5c2a40
Asek3 <64613894+Asek3@users.noreply.github.com>
2022-08-11 15:49:48 +0300

    Revert Eidolon compatibility code
    Fixes #204

--- src/main/java/me/jellysquid/mods/sodium/mixin/core/pipeline/MixinBufferBuilder.java
+++ src/main/java/me/jellysquid/mods/sodium/mixin/core/pipeline/MixinBufferBuilder.java
@@ -16,8 +16,6 @@ import org.slf4j.Logger;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 import java.nio.ByteBuffer;
 
@@ -43,14 +41,6 @@ public abstract class MixinBufferBuilder implements VertexBufferView, VertexDrai
 
     @Shadow
     private int vertexCount;
-
-    @Redirect(method = "popData", at = @At(value = "INVOKE", target = "Ljava/nio/ByteBuffer;limit(I)Ljava/nio/ByteBuffer;"))
-    public ByteBuffer debugGetNextBuffer(ByteBuffer buffer, int newLimit) {
-        ensureBufferCapacity(newLimit);
-        buffer = this.buffer;
-        buffer.limit(newLimit);
-        return buffer;
-    }
     
     @Override
     public boolean ensureBufferCapacity(int bytes) {
