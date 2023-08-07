71c6979038275aa0a91d7a9f740e12fe350b80e3
Bogdan <64613894+Asek3@users.noreply.github.com>
2023-08-07 14:34:20 +0300

    Fixed Vignette option
    Should be also applied to 1.19.x branches

---
+++ src/main/java/me/jellysquid/mods/sodium/mixin/features/options/overlays/VanillaGuiOverlayMixin.java
@@ -0,0 +1,17 @@
+package me.jellysquid.mods.sodium.mixin.features.options.overlays;
+
+import me.jellysquid.mods.sodium.client.SodiumClientMod;
+import net.minecraftforge.client.gui.overlay.VanillaGuiOverlay;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(VanillaGuiOverlay.class)
+public class VanillaGuiOverlayMixin {
+
+    @Redirect(method = "lambda$static$0", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/MinecraftClient;isFancyGraphicsOrBetter()Z"))
+    private static boolean redirectFancyGraphicsVignette() {
+        return SodiumClientMod.options().quality.enableVignette;
+    }
+
+}
