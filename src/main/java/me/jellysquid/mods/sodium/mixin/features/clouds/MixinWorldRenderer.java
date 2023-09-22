f07bb1d6cf2a4568f0d80765849160824d617727
Asek3 <64613894+Asek3@users.noreply.github.com>
2023-09-22 14:18:14 +0300

    Priority for Oculus

--- src/main/java/me/jellysquid/mods/sodium/mixin/features/clouds/MixinWorldRenderer.java
+++ src/main/java/me/jellysquid/mods/sodium/mixin/features/clouds/MixinWorldRenderer.java
@@ -17,7 +17,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(WorldRenderer.class)
+@Mixin(value = WorldRenderer.class, priority = 990)
 public class MixinWorldRenderer {
     @Shadow
     private @Nullable ClientWorld world;
