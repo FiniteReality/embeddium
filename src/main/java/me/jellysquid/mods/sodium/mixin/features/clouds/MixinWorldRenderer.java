83acea96a704343cc748bee4da56184591c702cb
Asek3 <64613894+Asek3@users.noreply.github.com>
2023-09-22 14:00:56 +0300

    Support for Forge custom cloud rendering
    Fixes #531
    Also should be applied on 1.20.x branches

--- src/main/java/me/jellysquid/mods/sodium/mixin/features/clouds/MixinWorldRenderer.java
+++ src/main/java/me/jellysquid/mods/sodium/mixin/features/clouds/MixinWorldRenderer.java
@@ -36,11 +36,13 @@ public class MixinWorldRenderer {
      */
     @Overwrite
     public void renderClouds(MatrixStack matrices, Matrix4f projectionMatrix, float tickDelta, double x, double y, double z) {
-        if (this.cloudRenderer == null) {
-            this.cloudRenderer = new CloudRenderer(client.getResourceManager());
-        }
+        if (!this.world.getDimensionEffects().renderClouds(this.world, this.ticks, tickDelta, matrices, x, y, z, projectionMatrix)) {
+            if (this.cloudRenderer == null) {
+                this.cloudRenderer = new CloudRenderer(client.getResourceManager());
+            }
 
-        this.cloudRenderer.render(this.world, this.client.player, matrices, projectionMatrix, this.ticks, tickDelta, x, y, z);
+            this.cloudRenderer.render(this.world, this.client.player, matrices, projectionMatrix, this.ticks, tickDelta, x, y, z);
+        }
     }
 
     @Inject(method = "reload(Lnet/minecraft/resource/ResourceManager;)V", at = @At("RETURN"))
