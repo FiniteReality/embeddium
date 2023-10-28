a4ad97045e44c5399c22d816a6fc08e0097d72ce
embeddedt <42941056+embeddedt@users.noreply.github.com>
2023-10-28 11:33:30 -0400

    Update vignette injection point for 1.19

--- src/main/java/me/jellysquid/mods/sodium/mixin/features/options/MixinInGameHud.java
+++ src/main/java/me/jellysquid/mods/sodium/mixin/features/options/MixinInGameHud.java
@@ -1,12 +1,12 @@
 package me.jellysquid.mods.sodium.mixin.features.options;
 
 import me.jellysquid.mods.sodium.client.SodiumClientMod;
-import net.minecraftforge.client.gui.ForgeIngameGui;
+import net.minecraftforge.client.gui.overlay.VanillaGuiOverlay;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(ForgeIngameGui.class)
+@Mixin(VanillaGuiOverlay.class)
 public class MixinInGameHud {
     @Redirect(method = "lambda$static$0", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/MinecraftClient;isFancyGraphicsOrBetter()Z"))
     private static boolean redirectFancyGraphicsVignette() {
