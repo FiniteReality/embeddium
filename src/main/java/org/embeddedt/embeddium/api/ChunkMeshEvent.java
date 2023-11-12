b3c7912e428eb0e51093fd9d6518631a40bcd7e7
embeddedt <42941056+embeddedt@users.noreply.github.com>
2023-11-12 14:14:20 -0500

    Document ChunkMeshEvent

--- src/main/java/org/embeddedt/embeddium/api/ChunkMeshEvent.java
+++ src/main/java/org/embeddedt/embeddium/api/ChunkMeshEvent.java
@@ -1,6 +1,7 @@
 package org.embeddedt.embeddium.api;
 
 import net.minecraft.util.math.ChunkSectionPos;
+import net.minecraft.world.BlockRenderView;
 import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.Event;
@@ -11,6 +12,18 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
+/**
+ * This event is fired by Embeddium to allow mods to add their own custom geometry to chunks as they are being meshed.
+ *
+ * <p>In vanilla, mods often accomplish this by using a <a href="https://github.com/BluSunrize/ImmersiveEngineering/blob/1.18.2/src/main/java/blusunrize/immersiveengineering/mixin/coremods/client/RebuildTaskMixin.java">mixin</a></a>
+ * into {@link net.minecraft.client.render.chunk.ChunkBuilder.BuiltChunk.RebuildTask}, but that is hacky and does not
+ * work with Embeddium as it doesn't use vanilla's chunk task classes.
+ *
+ * <p>This event is fired on the main thread, and provides the world being rendered and the section position. If you
+ * wish to add additional static geometry for this section, you should register your {@link MeshAppender} inside the
+ * event handler. Note that {@link MeshAppender#render(MeshAppender.Context)} will be called on a worker thread, and
+ * therefore accessing the world is not safe inside the appender itself. Instead, use the provided {@link BlockRenderView}.
+ */
 public class ChunkMeshEvent extends Event {
     private final World world;
     private final ChunkSectionPos sectionOrigin;
@@ -21,6 +34,9 @@ public class ChunkMeshEvent extends Event {
         this.sectionOrigin = sectionOrigin;
     }
 
+    /**
+     * Retrieves the current world (not cloned chunk section).
+     */
     public World getWorld() {
         return world;
     }
@@ -29,6 +45,11 @@ public class ChunkMeshEvent extends Event {
         return sectionOrigin;
     }
 
+    /**
+     * Register a mesh appender to be called when this section is meshed. The appender will be called on a worker thread,
+     * so it must not attempt to access shared client data that is not thread-safe (e.g. the full client world).
+     * @param appender the mesh appender to add
+     */
     public void addMeshAppender(MeshAppender appender) {
         if (meshAppenders == null) {
             meshAppenders = new ArrayList<>();
--- src/main/java/org/embeddedt/embeddium/api/MeshAppender.java
+++ src/main/java/org/embeddedt/embeddium/api/MeshAppender.java
@@ -9,8 +9,22 @@ import net.minecraft.world.BlockRenderView;
 import java.util.function.Function;
 
 public interface MeshAppender {
+    /**
+     * Called to add appropriate geometry to the section.
+     * @param context render context for this section
+     */
     void render(Context context);
 
+    /**
+     * Section rendering context for a MeshAppender.
+     * @param vertexConsumerProvider Provides access to {@link VertexConsumer}s for each render layer. Any vertices
+     *                               pumped into these vertex consumers will be added to the chunk's final mesh
+     * @param blockRenderView The chunk section being rendered. You should only retrieve blocks using this, not the
+     *                        client world
+     * @param sectionOrigin The origin of the section in the world
+     * @param sodiumBuildBuffers Provides access to the Sodium/Embeddium vertex writing APIs. Intended mainly for internal
+     *                           use
+     */
     record Context(Function<RenderLayer, VertexConsumer> vertexConsumerProvider,
                    BlockRenderView blockRenderView,
                    ChunkSectionPos sectionOrigin,
