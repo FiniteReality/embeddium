e736ab79979ea0641c4ac2c65da9da7fc579eb05
embeddedt <42941056+embeddedt@users.noreply.github.com>
2023-11-13 12:58:12 -0500

    Avoid crash in ModelQuadFlags with incorrectly sized quads

--- src/main/java/me/jellysquid/mods/sodium/client/model/quad/properties/ModelQuadFlags.java
+++ src/main/java/me/jellysquid/mods/sodium/client/model/quad/properties/ModelQuadFlags.java
@@ -44,7 +44,8 @@ public class ModelQuadFlags {
         float maxY = -32.0F;
         float maxZ = -32.0F;
 
-        for (int i = 0; i < 4; ++i) {
+        int numVertices = bakedQuad.getVertexData().length / 8;
+        for (int i = 0; i < numVertices; ++i) {
             float x = quad.getX(i);
             float y = quad.getY(i);
             float z = quad.getZ(i);
