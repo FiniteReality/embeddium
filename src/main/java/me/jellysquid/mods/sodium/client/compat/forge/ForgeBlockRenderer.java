480c56bd24aedf3da531d85bfebca5129da15dbd
embeddedt <42941056+embeddedt@users.noreply.github.com>
2023-11-16 14:08:04 -0500

    Remove copy of Forge block renderer code

--- src/main/java/me/jellysquid/mods/sodium/client/compat/forge/ForgeBlockRenderer.java
+++ src/main/java/me/jellysquid/mods/sodium/client/compat/forge/ForgeBlockRenderer.java
@@ -7,8 +7,8 @@ import me.jellysquid.mods.sodium.client.render.occlusion.BlockOcclusionCache;
 import me.jellysquid.mods.sodium.common.util.DirectionUtil;
 import net.minecraft.block.BlockState;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.color.block.BlockColors;
 import net.minecraft.client.render.VertexConsumer;
+import net.minecraft.client.render.block.BlockModelRenderer;
 import net.minecraft.client.render.model.BakedModel;
 import net.minecraft.client.render.model.BakedQuad;
 import net.minecraft.client.texture.Sprite;
@@ -17,95 +17,62 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.BlockRenderView;
 import net.minecraftforge.client.model.data.IModelData;
-import net.minecraftforge.client.model.pipeline.VertexBufferConsumer;
-import net.minecraftforge.client.model.pipeline.VertexLighterFlat;
-import net.minecraftforge.client.model.pipeline.VertexLighterSmoothAo;
 import net.minecraftforge.common.ForgeConfig;
 
 import java.util.List;
 import java.util.Random;
 
 /**
- * Utility class for BlockRenderer, that uses the Forge lighting pipeline.
- *
- * This class is derived from Forge's renderer, and is licensed under LGPL-2.1. As the class is a standalone class file,
- * it may be replaced in an existing copy of Embeddium with an alternate LGPL-2.1 implementation.
+ * Utility class for BlockRenderer, that delegates to the Forge lighting pipeline.
  */
 public class ForgeBlockRenderer {
-    private final BlockColors colors = MinecraftClient.getInstance().getBlockColors();
-    private final ThreadLocal<VertexLighterFlat> lighterFlat = ThreadLocal.withInitial(() -> new VertexLighterFlat(colors));
-    private final ThreadLocal<VertexLighterSmoothAo> lighterSmooth = ThreadLocal.withInitial(() -> new VertexLighterSmoothAo(colors));
-    private final ThreadLocal<VertexBufferConsumer> consumerFlat = ThreadLocal.withInitial(VertexBufferConsumer::new);
-    private final ThreadLocal<VertexBufferConsumer> consumerSmooth = ThreadLocal.withInitial(VertexBufferConsumer::new);
-
-
     private static boolean useForgeLightingPipeline = false;
+    private static BlockModelRenderer forgeRenderer;
 
     public static void init() {
         useForgeLightingPipeline = ForgeConfig.CLIENT.experimentalForgeLightPipelineEnabled.get();
+        forgeRenderer = MinecraftClient.getInstance().getBlockRenderManager().getModelRenderer();
     }
 
     public static boolean useForgeLightingPipeline() {
         return useForgeLightingPipeline;
     }
 
-    private void processQuad(ChunkRenderData.Builder renderData, BakedQuad quad) {
-        ModelQuadView src = (ModelQuadView)quad;
-        Sprite sprite = src.rubidium$getSprite();
-
-        if (sprite != null) {
-            renderData.addSprite(sprite);
+    private boolean markQuads(ChunkRenderData.Builder renderData, List<BakedQuad> quads) {
+        if (quads.isEmpty()) {
+            return true;
         }
+        for (int i = 0; i < quads.size(); i++) {
+            ModelQuadView src = (ModelQuadView)quads.get(i);
+            Sprite sprite = src.rubidium$getSprite();
+
+            if (sprite != null) {
+                renderData.addSprite(sprite);
+            }
+        }
+        return false;
     }
 
     public boolean renderBlock(LightMode mode, BlockState state, BlockPos pos, BlockRenderView world, BakedModel model, MatrixStack stack,
                                VertexConsumer buffer, Random random, long seed, IModelData data, boolean checkSides, BlockOcclusionCache sideCache,
                                ChunkRenderData.Builder renderData) {
-        VertexBufferConsumer consumer = mode == LightMode.FLAT ? this.consumerFlat.get() : this.consumerSmooth.get();
-        consumer.setBuffer(buffer);
-        VertexLighterFlat lighter = mode == LightMode.FLAT ? this.lighterFlat.get() : this.lighterSmooth.get();
-        lighter.setParent(consumer);
-        lighter.setTransform(stack.peek());
+        if (mode == LightMode.FLAT) {
+            forgeRenderer.renderModelFlat(world, model, state, pos, stack, buffer, checkSides, random, seed, 0, data);
+        } else {
+            forgeRenderer.renderModelSmooth(world, model, state, pos, stack, buffer, checkSides, random, seed, 0, data);
+        }
 
-        // render
-        lighter.setWorld(world);
-        lighter.setState(state);
-        lighter.setBlockPos(pos);
-        boolean empty = true;
+        // Process the quads a second time for marking animated sprites and detecting emptiness
+        boolean empty;
+
         random.setSeed(seed);
+        empty = markQuads(renderData, model.getQuads(state, null, random, data));
 
-        List<BakedQuad> quads = model.getQuads(state, null, random, data);
-        if(!quads.isEmpty()) {
-            lighter.updateBlockInfo();
-            empty = false;
-            // noinspection ForLoopReplaceableByForEach
-            for(int i = 0; i < quads.size(); i++) {
-                BakedQuad quad = quads.get(i);
-                quad.pipe(lighter);
-                processQuad(renderData, quad);
-            }
-        }
-
-        for(Direction side : DirectionUtil.ALL_DIRECTIONS)
-        {
+        for(Direction side : DirectionUtil.ALL_DIRECTIONS) {
             random.setSeed(seed);
-            quads = model.getQuads(state, side, random, data);
-            if(!quads.isEmpty())
-            {
-                if(!checkSides || sideCache.shouldDrawSide(state, world, pos, side))
-                {
-                    if(empty) lighter.updateBlockInfo();
-                    empty = false;
-                    // noinspection ForLoopReplaceableByForEach
-                    for(int i = 0; i < quads.size(); i++) {
-                        BakedQuad quad = quads.get(i);
-                        quad.pipe(lighter);
-                        processQuad(renderData, quad);
-                    }
-                }
-            }
+            empty = markQuads(renderData, model.getQuads(state, side, random, data));
         }
-        lighter.resetBlockInfo();
+
         return !empty;
     }
 }
