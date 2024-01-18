124a76c6a01f767a39c58925cf89dfe60ddbd86d
JellySquid <jellysquid@pm.me>
2024-01-18 01:21:41 -0600

    Ensure strict ordering of regions during occlusion culling

--- src/main/java/me/jellysquid/mods/sodium/client/render/chunk/lists/VisibleChunkCollector.java
+++ src/main/java/me/jellysquid/mods/sodium/client/render/chunk/lists/VisibleChunkCollector.java
@@ -1,19 +1,25 @@
 package me.jellysquid.mods.sodium.client.render.chunk.lists;
 
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import me.jellysquid.mods.sodium.client.render.chunk.ChunkUpdateType;
 import me.jellysquid.mods.sodium.client.render.chunk.RenderSection;
 import java.util.ArrayDeque;
 import java.util.EnumMap;
 import java.util.Map;
 import java.util.Queue;
-import java.util.function.Consumer;
+import me.jellysquid.mods.sodium.client.render.chunk.occlusion.OcclusionCuller;
+import me.jellysquid.mods.sodium.client.render.chunk.region.RenderRegion;
 
-public class VisibleChunkCollector implements Consumer<RenderSection> {
-    private final SortedRenderLists.Builder sortedRenderLists;
+public class VisibleChunkCollector implements OcclusionCuller.Visitor {
+    private final ObjectArrayList<ChunkRenderList> sortedRenderLists;
     private final EnumMap<ChunkUpdateType, ArrayDeque<RenderSection>> sortedRebuildLists;
 
+    private final int frame;
+
     public VisibleChunkCollector(int frame) {
-        this.sortedRenderLists = new SortedRenderLists.Builder(frame);
+        this.frame = frame;
+
+        this.sortedRenderLists = new ObjectArrayList<>();
         this.sortedRebuildLists = new EnumMap<>(ChunkUpdateType.class);
 
         for (var type : ChunkUpdateType.values()) {
@@ -22,8 +28,21 @@ public class VisibleChunkCollector implements Consumer<RenderSection> {
     }
 
     @Override
-    public void accept(RenderSection section) {
-        this.sortedRenderLists.add(section);
+    public void visit(RenderSection section, boolean visible) {
+        RenderRegion region = section.getRegion();
+        ChunkRenderList renderList = region.getRenderList();
+
+        // Even if a section does not have render objects, we must ensure the render list is initialized and put
+        // into the sorted queue of lists, so that we maintain the correct order of draw calls.
+        if (renderList.getLastVisibleFrame() != this.frame) {
+            renderList.reset(this.frame);
+
+            this.sortedRenderLists.add(renderList);
+        }
+
+        if (visible && section.getFlags() != 0) {
+            renderList.add(section);
+        }
 
         this.addToRebuildLists(section);
     }
@@ -41,7 +60,7 @@ public class VisibleChunkCollector implements Consumer<RenderSection> {
     }
 
     public SortedRenderLists createRenderLists() {
-        return this.sortedRenderLists.build();
+        return new SortedRenderLists(this.sortedRenderLists);
     }
 
     public Map<ChunkUpdateType, ArrayDeque<RenderSection>> getRebuildLists() {
