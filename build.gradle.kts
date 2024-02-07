efc5fff4ef53588a536fff9eb35e16120b003c82
Nolij <xdmatthewbx@mixhosting.xyz>
2024-02-07 09:56:13 -0500

    fix indentation + new maven

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,14 @@
 plugins {
-	id("idea")
-	id("net.neoforged.gradle") version("6.0.21")
-	id("maven-publish")
-	id("org.spongepowered.mixin") version("0.7.38")
+    id("idea")
+    id("net.neoforged.gradle") version("6.0.21")
+    id("maven-publish")
+    id("org.spongepowered.mixin") version("0.7.38")
 
     // This dependency is only used to determine the state of the Git working tree so that build artifacts can be
     // more easily identified. TODO: Lazily load GrGit via a service only when builds are performed.
     id("org.ajoberstar.grgit") version("5.0.0")
 
-	id("me.modmuss50.mod-publish-plugin") version("0.3.4")
+    id("me.modmuss50.mod-publish-plugin") version("0.3.4")
 }
 
 operator fun String.invoke(): String {
@@ -25,7 +25,7 @@ version = "${"mod_version"()}${getVersionMetadata()}+mc${"minecraft_version"()}"
 group = "maven_group"()
 
 base {
-	archivesName = "archives_base_name"()
+    archivesName = "archives_base_name"()
 }
 
 // Mojang ships Java 17 to end users in 1.18+, so your mod should target Java 17.
@@ -66,58 +66,58 @@ sourceSets {
 }
 
 repositories {
-	maven("https://maven.minecraftforge.net/")
-	maven("https://maven.fabricmc.net")
+    maven("https://maven.minecraftforge.net/")
+    maven("https://maven.fabricmc.net")
     maven("https://maven.tterrag.com/")
     maven("https://maven.blamejared.com")
-	maven("https://api.modrinth.com/maven") {
+    maven("https://api.modrinth.com/maven") {
         content {
             includeGroup("maven.modrinth")
         }
     }
-	maven("https://cursemaven.com") {
-		content {
-			includeGroup("curse.maven")
-		}
-	}
-	maven { url = "https://maven.covers1624.net/" }
+    maven("https://cursemaven.com") {
+        content {
+            includeGroup("curse.maven")
+        }
+    }
+    maven("https://maven.covers1624.net/")
 }
 
 
 minecraft {
-	mappings("official", "minecraft_version"())
-	copyIdeResources = true
-	accessTransformer(file("src/main/resources/META-INF/accesstransformer.cfg"))
-	runs {
-		configureEach {
-			workingDirectory(project.file("run"))
+    mappings("official", "minecraft_version"())
+    copyIdeResources = true
+    accessTransformer(file("src/main/resources/META-INF/accesstransformer.cfg"))
+    runs {
+        configureEach {
+            workingDirectory(project.file("run"))
 
-			property("forge.logging.console.level", "info")
+            property("forge.logging.console.level", "info")
 
-			property("mixin.env.remapRefMap", "true")
-			property("mixin.env.refMapRemappingFile", "${projectDir}/build/createSrgToMcp/output.srg")
+            property("mixin.env.remapRefMap", "true")
+            property("mixin.env.refMapRemappingFile", "${projectDir}/build/createSrgToMcp/output.srg")
 
-			mods {
-				create("embeddium") {
-					sources(sourceSets["main"])
+            mods {
+                create("embeddium") {
+                    sources(sourceSets["main"])
                     sources(sourceSets["compat"])
                     sources(sourceSets["api"])
                     sources(sourceSets["legacy"])
-				}
-			}
-		}
+                }
+            }
+        }
 
-		create("client") {}
-	}
+        create("client") {}
+    }
 }
 
 configurations {
-	val runtimeOnlyNonPublishable = create("runtimeOnlyNonPublishable") {
-		description = "Runtime only dependencies that are not published alongside the jar"
-		isCanBeConsumed = false
-		isCanBeResolved = false
-	}
-	runtimeClasspath.get().extendsFrom(runtimeOnlyNonPublishable)
+    val runtimeOnlyNonPublishable = create("runtimeOnlyNonPublishable") {
+        description = "Runtime only dependencies that are not published alongside the jar"
+        isCanBeConsumed = false
+        isCanBeResolved = false
+    }
+    runtimeClasspath.get().extendsFrom(runtimeOnlyNonPublishable)
 }
 
 val extraModsDir = "extra-mods-${"minecraft_version"()}"
@@ -130,9 +130,9 @@ repositories {
 }
 
 mixin {
-	// MixinGradle Settings
-	add(sourceSets["main"], "embeddium-refmap.json")
-	config("embeddium.mixins.json")
+    // MixinGradle Settings
+    add(sourceSets["main"], "embeddium-refmap.json")
+    config("embeddium.mixins.json")
 }
 
 fun DependencyHandlerScope.compatCompileOnly(dependency: Dependency) {
@@ -140,56 +140,56 @@ fun DependencyHandlerScope.compatCompileOnly(dependency: Dependency) {
 }
 
 dependencies {
-	minecraft("net.minecraftforge:forge:${"minecraft_version"()}-${"forge_version"()}")
+    minecraft("net.minecraftforge:forge:${"minecraft_version"()}-${"forge_version"()}")
 
     // Mods
     compatCompileOnly(fg.deobf("curse.maven:codechickenlib-242818:${"codechicken_fileid"()}"))
     compatCompileOnly(fg.deobf("curse.maven:immersiveengineering-231951:${"ie_fileid"()}"))
 
-	annotationProcessor("net.fabricmc:sponge-mixin:0.12.5+mixin.0.8.5")
+    annotationProcessor("net.fabricmc:sponge-mixin:0.12.5+mixin.0.8.5")
 
-	// runtime remapping at home
+    // runtime remapping at home
     fileTree(extraModsDir) {
         include("*.jar") 
     }.files.forEach { extraModJar ->
-		val basename = extraModJar.name.substring(0, extraModJar.name.length - ".jar".length)
-		val versionSep = basename.lastIndexOf('-')
-		assert(versionSep != -1)
-		val artifactId = basename.substring(0, versionSep)
-		val version = basename.substring(versionSep + 1)
-		runtimeOnly(fg.deobf("extra-mods:$artifactId:$version"))
-	}
+        val basename = extraModJar.name.substring(0, extraModJar.name.length - ".jar".length)
+        val versionSep = basename.lastIndexOf('-')
+        assert(versionSep != -1)
+        val artifactId = basename.substring(0, versionSep)
+        val version = basename.substring(versionSep + 1)
+        runtimeOnly(fg.deobf("extra-mods:$artifactId:$version"))
+    }
 }
 
 tasks.processResources {
-	inputs.property("version", "version"())
+    inputs.property("version", "version"())
 
-	filesMatching("META-INF/mods.toml") {
+    filesMatching("META-INF/mods.toml") {
         expand("version" to "version"())
     }
 }
 
 tasks.withType<JavaCompile> {
-	options.release = 17
+    options.release = 17
 }
 
 
 java {
-	withSourcesJar()
+    withSourcesJar()
 }
 
 tasks.jar {
-	from("LICENSE") {
-		rename { "${it}_${"archives_base_name"()}"}
-	}
-	from(sourceSets["compat"].output.classesDirs)
-	from(sourceSets["compat"].output.resourcesDir)
+    from("LICENSE") {
+        rename { "${it}_${"archives_base_name"()}"}
+    }
+    from(sourceSets["compat"].output.classesDirs)
+    from(sourceSets["compat"].output.resourcesDir)
     from(sourceSets["api"].output.classesDirs)
     from(sourceSets["api"].output.resourcesDir)
     from(sourceSets["legacy"].output.classesDirs)
     from(sourceSets["legacy"].output.resourcesDir)
 
-	finalizedBy("reobfJar")
+    finalizedBy("reobfJar")
 }
 
 tasks.named<Jar>("sourcesJar").configure {
@@ -199,55 +199,55 @@ tasks.named<Jar>("sourcesJar").configure {
 }
 
 publishing {
-	tasks.publish {
+    tasks.publish {
         dependsOn(tasks.build)
     }
-	publications {
+    publications {
         this.create<MavenPublication>("mavenJava") {
             artifact(tasks.jar)
             artifact(tasks.named("sourcesJar"))
         }
-	}
+    }
 
-	repositories {
-		maven("file://${System.getenv("local_maven")}")
-	}
+    repositories {
+        maven("file://${System.getenv("local_maven")}")
+    }
 }
 
 publishMods {
-	file = tasks.jar.get().archiveFile
-	changelog = "https://github.com/embeddedt/embeddium/wiki/Changelog"
-	type = STABLE
+    file = tasks.jar.get().archiveFile
+    changelog = "https://github.com/embeddedt/embeddium/wiki/Changelog"
+    type = STABLE
     modLoaders.add("forge")
     modLoaders.add("neoforge")
 
-	curseforge {
-		projectId = "908741"
-		accessToken = providers.environmentVariable("CURSEFORGE_TOKEN")
-		minecraftVersions.add("minecraft_version"())
+    curseforge {
+        projectId = "908741"
+        accessToken = providers.environmentVariable("CURSEFORGE_TOKEN")
+        minecraftVersions.add("minecraft_version"())
 
-		incompatible {
-			slug = "rubidium"
-		}
-	}
-	modrinth {
-		projectId = "sk9rgfiA"
-		accessToken = providers.environmentVariable("MODRINTH_TOKEN")
-		minecraftVersions.add("minecraft_version"())
+        incompatible {
+            slug = "rubidium"
+        }
+    }
+    modrinth {
+        projectId = "sk9rgfiA"
+        accessToken = providers.environmentVariable("MODRINTH_TOKEN")
+        minecraftVersions.add("minecraft_version"())
 
-		incompatible {
-			slug = "rubidium"
-		}
-	}
+        incompatible {
+            slug = "rubidium"
+        }
+    }
 
-	displayName = "[${"minecraft_version"()}] Embeddium ${"mod_version"()}"
+    displayName = "[${"minecraft_version"()}] Embeddium ${"mod_version"()}"
 }
 
 fun getVersionMetadata(): String {
-	// CI builds only
-	if (project.hasProperty("build.release")) {
-		return "" // no tag whatsoever
-	}
+    // CI builds only
+    if (project.hasProperty("build.release")) {
+        return "" // no tag whatsoever
+    }
 
     val head = grgit.head()
     var id = head.abbreviatedId
