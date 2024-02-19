7a40cf86e68b7ae1b312612baa1d67c88c1b8c25
embeddedt <42941056+embeddedt@users.noreply.github.com>
2024-02-18 19:54:30 -0500

    Partially rewrite mipmap changes
    The original texture is no longer modified in-place, so vanilla
    leaves look reasonable in Fast graphics again

--- src/main/java/me/jellysquid/mods/sodium/mixin/features/textures/mipmaps/MipmapHelperMixin.java
+++ src/main/java/me/jellysquid/mods/sodium/mixin/features/textures/mipmaps/MipmapHelperMixin.java
@@ -1,17 +1,31 @@
 package me.jellysquid.mods.sodium.mixin.features.textures.mipmaps;
 
+import com.llamalad7.mixinextras.sugar.Share;
+import com.llamalad7.mixinextras.sugar.ref.LocalRef;
+import com.mojang.blaze3d.platform.NativeImage;
+import me.jellysquid.mods.sodium.client.util.NativeImageHelper;
 import me.jellysquid.mods.sodium.client.util.color.ColorSRGB;
 import net.minecraft.client.renderer.texture.MipmapGenerator;
 import net.minecraft.util.FastColor;
+import org.lwjgl.system.MemoryUtil;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import java.util.OptionalInt;
 
 /**
  * Implements a significantly enhanced mipmap downsampling filter.
  *
- * <p>This algorithm combines ideas from vanilla Minecraft -- using linear color spaces instead of sRGB for blending) --
- * with ideas from OptiFine -- using the alpha values for weighting in downsampling -- to produce a novel downsampling
+ * <p>This algorithm combines:
+ * <li>
+ *     <ul>ideas from vanilla Minecraft -- using linear color spaces instead of sRGB for blending)</ul>
+ *     <ul>ideas from OptiFine -- using the alpha values for weighting in downsampling</ul>
+ *     <ul>ideas from Sodium -- replacing the transparent black pixels in textures with the weighted average color of non-transparent pixels</ul>
+ * </li>
+ * to produce a novel downsampling
  * algorithm for mipmapping that produces minimal visual artifacts.</p>
  *
  * <p>This implementation fixes a number of issues with other implementations:</p>
@@ -22,9 +36,11 @@ import org.spongepowered.asm.mixin.Unique;
  *     <ul>Vanilla computes a simple average of the 4 pixels, disregarding the relative alpha values of pixels. In
  *         cutout textures, this results in a lot of pixels with high alpha values and dark colors, causing visual
  *         artifacts.</ul>
+ *     <ul>Sodium replaces the transparent black pixels on the original texture (not just when mipmapping), which causes leaves to look
+ *     bad in Fast graphics without a texture replacement. </ul>
  * </li>
  *
- * This Mixin is ported from Iris at <a href="https://github.com/IrisShaders/Iris/blob/41095ac23ea0add664afd1b85c414d1f1ed94066/src/main/java/net/coderbot/iris/mixin/bettermipmaps/MixinMipmapGenerator.java">MixinMipmapGenerator</a>.
+ * This mixin is based on original work from Iris at <a href="https://github.com/IrisShaders/Iris/blob/41095ac23ea0add664afd1b85c414d1f1ed94066/src/main/java/net/coderbot/iris/mixin/bettermipmaps/MixinMipmapGenerator.java">MixinMipmapGenerator</a>.
  */
 @Mixin(MipmapGenerator.class)
 public class MipmapHelperMixin {
@@ -38,6 +54,84 @@ public class MipmapHelperMixin {
         //
         // This works well for the case where our change is the most impactful (grass side overlays)
         return weightedAverageColor(weightedAverageColor(one, two), weightedAverageColor(three, four));
+    }
+
+    /**
+     * @author embeddedt
+     * @reason Compute the average color of this texture while checking if it contains transparent pixels.
+     */
+    @Redirect(method = "generateMipLevels", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/texture/MipmapGenerator;hasTransparentPixel(Lcom/mojang/blaze3d/platform/NativeImage;)Z"))
+    private static boolean storeAverageColorIfTransparent(NativeImage image, @Share("averageColor") LocalRef<OptionalInt> averageColorRef) {
+        var averageColor = sodium$computeAveragePixelColor(image);
+        averageColorRef.set(averageColor);
+        return averageColor.isPresent();
+    }
+
+    /**
+     * @author embeddedt
+     * @reason When retrieving pixels from the original texture (level 0), check if the pixel has alpha 0,
+     * and if so, use the average color in its place.
+     */
+    @Redirect(method = "generateMipLevels", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/platform/NativeImage;getPixelRGBA(II)I"))
+    private static int getPixelOrTransparent(NativeImage image, int x, int y, NativeImage[] imageArray, @Share("averageColor") LocalRef<OptionalInt> averageColorRef) {
+        int originalPixel = image.getPixelRGBA(x, y);
+        if(image == imageArray[0] && FastColor.ABGR32.alpha(originalPixel) == 0) {
+            return averageColorRef.get().orElse(originalPixel);
+        } else {
+            return originalPixel;
+        }
+    }
+
+    /**
+     * Compute the average pixel color of all non-transparent pixels in nativeImage. If the image
+     * doesn't contain any fully transparent pixels, returns an empty optional int.
+     * @param nativeImage the image to read pixels from
+     * @return the average color, or empty if there are no fully transparent pixels
+     */
+    @Unique
+    private static OptionalInt sodium$computeAveragePixelColor(NativeImage nativeImage) {
+        final long ppPixel = NativeImageHelper.getPointerRGBA(nativeImage);
+        final int pixelCount = nativeImage.getHeight() * nativeImage.getWidth();
+
+        // Calculate an average color from all pixels that are not completely transparent.
+        // This average is weighted based on the (non-zero) alpha value of the pixel.
+        float r = 0.0f;
+        float g = 0.0f;
+        float b = 0.0f;
+
+        float totalWeight = 0.0f;
+
+        for (int pixelIndex = 0; pixelIndex < pixelCount; pixelIndex++) {
+            long pPixel = ppPixel + (pixelIndex * 4);
+
+            int color = MemoryUtil.memGetInt(pPixel);
+            int alpha = FastColor.ABGR32.alpha(color);
+
+            // Ignore all fully-transparent pixels for the purposes of computing an average color.
+            if (alpha != 0) {
+                float weight = (float) alpha;
+
+                // Make sure to convert to linear space so that we don't lose brightness.
+                r += ColorSRGB.srgbToLinear(FastColor.ABGR32.red(color)) * weight;
+                g += ColorSRGB.srgbToLinear(FastColor.ABGR32.green(color)) * weight;
+                b += ColorSRGB.srgbToLinear(FastColor.ABGR32.blue(color)) * weight;
+
+                totalWeight += weight;
+            }
+        }
+
+        // Bail if none of the pixels are semi-transparent.
+        if (totalWeight == 0.0f) {
+            return OptionalInt.empty();
+        }
+
+        r /= totalWeight;
+        g /= totalWeight;
+        b /= totalWeight;
+
+        // Convert that color in linear space back to sRGB.
+        // Use an alpha value of zero - this works since we only replace pixels with an alpha value of 0.
+        return OptionalInt.of(ColorSRGB.linearToSrgb(r, g, b, 0));
     }
 
     @Unique
