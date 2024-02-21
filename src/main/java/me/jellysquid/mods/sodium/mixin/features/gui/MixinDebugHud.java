74de52cf23f3231e46c9aa7b8e1860f9b13ef2a7
embeddedt <42941056+embeddedt@users.noreply.github.com>
2024-02-21 11:34:50 -0500

    Adjust debug HUD mixin for Forge 1.18 changes

--- src/main/java/me/jellysquid/mods/sodium/mixin/features/gui/MixinDebugHud.java
+++ src/main/java/me/jellysquid/mods/sodium/mixin/features/gui/MixinDebugHud.java
@@ -15,8 +15,8 @@ import net.minecraft.client.gui.Gui;
 import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.ItemRenderer;
-import net.minecraftforge.client.event.CustomizeGuiOverlayEvent;
-import net.minecraftforge.client.gui.overlay.ForgeGui;
+import net.minecraftforge.client.event.RenderGameOverlayEvent;
+import net.minecraftforge.client.gui.ForgeIngameGui;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -27,27 +27,32 @@ import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 import java.util.ArrayList;
 import java.util.List;
 
-@Mixin(ForgeGui.class)
+import static net.minecraftforge.client.event.RenderGameOverlayEvent.ElementType.TEXT;
+
+@Mixin(ForgeIngameGui.class)
 public abstract class MixinDebugHud extends Gui {
     @Shadow
     private Font font;
 
-    public MixinDebugHud(Minecraft p_232355_, ItemRenderer p_232356_) {
-        super(p_232355_, p_232356_);
+    @Shadow protected abstract void post(RenderGameOverlayEvent.ElementType type, PoseStack poseStack);
+
+    public MixinDebugHud(Minecraft p_93005_) {
+        super(p_93005_);
     }
 
     /**
      * @author embeddedt
      * @reason take over rendering of the actual list contents
      */
-    @Inject(method = "renderHUDText", at = @At(value = "INVOKE", target = "Lnet/minecraftforge/eventbus/api/IEventBus;post(Lnet/minecraftforge/eventbus/api/Event;)Z"), locals = LocalCapture.CAPTURE_FAILHARD, cancellable = true, remap = false)
-    private void embeddium$renderTextFast(int width, int height, PoseStack poseStack, CallbackInfo ci, ArrayList<String> listL, ArrayList<String> listR, CustomizeGuiOverlayEvent.DebugText event) {
+    @Inject(method = "renderHUDText", at = @At(value = "INVOKE", target = "Ljava/util/ArrayList;iterator()Ljava/util/Iterator;", ordinal = 0), locals = LocalCapture.CAPTURE_FAILHARD, cancellable = true, remap = false)
+    private void embeddium$renderTextFast(int width, int height, PoseStack poseStack, CallbackInfo ci, ArrayList<String> listL, ArrayList<String> listR, RenderGameOverlayEvent.Text event) {
         ci.cancel();
 
         renderForgeList(poseStack, listL, false);
         renderForgeList(poseStack, listR, true);
 
         minecraft.getProfiler().pop();
+        post(TEXT, poseStack);
     }
 
     private void renderForgeList(PoseStack matrixStack, List<String> list, boolean right) {
