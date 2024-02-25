8f99576259cb04c7b252474f27e3b18fdddc00cb
embeddedt <42941056+embeddedt@users.noreply.github.com>
2024-02-24 21:02:44 -0500

    Use our block occlusion cache for FRAPI blocks

--- src/main/java/me/jellysquid/mods/sodium/client/render/chunk/compile/pipeline/IndigoBlockRenderContext.java
+++ src/main/java/me/jellysquid/mods/sodium/client/render/chunk/compile/pipeline/IndigoBlockRenderContext.java
@@ -9,7 +9,9 @@ import me.jellysquid.mods.sodium.client.render.chunk.terrain.material.DefaultMat
 import net.fabricmc.fabric.impl.client.indigo.renderer.render.BlockRenderContext;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.core.Direction;
 import net.minecraft.util.RandomSource;
+import org.jetbrains.annotations.Nullable;
 import org.joml.Vector3fc;
 
 import java.util.Map;
@@ -20,6 +22,37 @@ import java.util.Map;
 public class IndigoBlockRenderContext extends BlockRenderContext {
     private final Map<RenderType, SinkingVertexBuilder> vertexBuilderMap = new Object2ObjectOpenHashMap<>();
 
+    private me.jellysquid.mods.sodium.client.render.chunk.compile.pipeline.BlockRenderContext currentContext;
+    private final BlockOcclusionCache occlusionCache;
+
+    private int cullChecked, cullValue;
+
+    public IndigoBlockRenderContext(BlockOcclusionCache occlusionCache) {
+        this.occlusionCache = occlusionCache;
+    }
+
+    @Override
+    public boolean isFaceCulled(@Nullable Direction face) {
+        if (face == null) {
+            return false;
+        }
+
+        int fM = (1 << face.ordinal());
+
+        // Use a bitmask to cache the cull checks so we don't run them more than once per face
+        if((cullChecked & fM) != 0) {
+            return (cullValue & fM) != 0;
+        } else {
+            var ctx = this.currentContext;
+            boolean flag = !this.occlusionCache.shouldDrawSide(ctx.state(), ctx.localSlice(), ctx.pos(), face);
+            if(flag) {
+                cullValue |= fM;
+            }
+            cullChecked |= fM;
+            return flag;
+        }
+    }
+
     @Override
     protected VertexConsumer getVertexConsumer(RenderType layer) {
         return vertexBuilderMap.computeIfAbsent(layer, k -> new SinkingVertexBuilder());
@@ -27,12 +60,19 @@ public class IndigoBlockRenderContext extends BlockRenderContext {
 
     public void reset() {
         vertexBuilderMap.values().forEach(SinkingVertexBuilder::reset);
+        cullChecked = 0;
+        cullValue = 0;
     }
 
     public void renderEmbeddium(me.jellysquid.mods.sodium.client.render.chunk.compile.pipeline.BlockRenderContext ctx,
                                 PoseStack mStack,
                                 RandomSource random) {
-        this.render(ctx.localSlice(), ctx.model(), ctx.state(), ctx.pos(), mStack, null, true, random, ctx.seed(), OverlayTexture.NO_OVERLAY);
+        this.currentContext = ctx;
+        try {
+            this.render(ctx.localSlice(), ctx.model(), ctx.state(), ctx.pos(), mStack, null, true, random, ctx.seed(), OverlayTexture.NO_OVERLAY);
+        } finally {
+            this.currentContext = null;
+        }
     }
 
     /**
