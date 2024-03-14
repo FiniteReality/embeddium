9bd08c4d30e33917720b792422bbc917c28a10b1
embeddedt <42941056+embeddedt@users.noreply.github.com>
2024-03-13 20:32:47 -0400

    Fix some modded content not rendering correctly in Fabulous

--- src/main/java/me/jellysquid/mods/sodium/mixin/core/render/world/WorldRendererMixin.java
+++ src/main/java/me/jellysquid/mods/sodium/mixin/core/render/world/WorldRendererMixin.java
@@ -129,7 +129,10 @@ public abstract class WorldRendererMixin implements WorldRendererExtended {
             RenderDevice.exitManagedCode();
         }
 
+        // TODO: Avoid setting up and clearing the state a second time
+        renderLayer.setupRenderState();
         ForgeHooksClient.dispatchRenderStage(renderLayer, ((LevelRenderer)(Object)this), matrices, matrix, this.ticks, this.minecraft.gameRenderer.getMainCamera(), this.getFrustum());
+        renderLayer.clearRenderState();
     }
 
     /**
