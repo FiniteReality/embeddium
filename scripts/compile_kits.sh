55c46dbf2237cc04e0699a21b130b07f62950def
embeddedt <42941056+embeddedt@users.noreply.github.com>
2024-03-15 14:48:29 -0400

    24w11a

---
+++ scripts/compile_kits.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+function prop {
+    grep "${1}" gradle.properties | cut -d'=' -f2
+}
+
+MC_VERSION=$(prop minecraft_version)
+
+if [[ $MC_VERSION == *"w"** ]]; then
+    branch=port/${MC_VERSION}
+    echo "Detected MC snapshot ${MC_VERSION}"
+    cd $(mktemp -d)
+    echo "Downloading Kits ${branch}..."
+    git clone -q -b ${branch} --depth 1 https://github.com/neoforged/NeoForge Kits >/dev/null
+    cd Kits
+    echo "Compiling Kits"
+    ./gradlew neoforge:setup neoforge:publishToMavenLocal
+fi
