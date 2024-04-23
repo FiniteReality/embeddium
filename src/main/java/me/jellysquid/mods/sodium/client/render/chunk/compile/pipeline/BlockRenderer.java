4ece91308065404241fcf8e95c401c8f1a862f15
embeddedt <42941056+embeddedt@users.noreply.github.com>
2024-04-22 20:01:43 -0400

    Rewrite Forge light pipeline handling
    All non-lighting-related optimizations are now re-enabled

--- src/main/java/me/jellysquid/mods/sodium/client/render/chunk/compile/pipeline/BlockRenderer.java
+++ src/main/java/me/jellysquid/mods/sodium/client/render/chunk/compile/pipeline/BlockRenderer.java
@@ -57,6 +57,7 @@ public class BlockRenderer {
     private final ChunkVertexEncoder.Vertex[] vertices = ChunkVertexEncoder.Vertex.uninitializedQuad();
 
     private final boolean useAmbientOcclusion;
+    @Deprecated(forRemoval = true)
     private final boolean useForgeExperimentalLightingPipeline;
 
     private final ForgeBlockRenderer forgeBlockRenderer = new ForgeBlockRenderer();
@@ -75,7 +76,7 @@ public class BlockRenderer {
 
         this.occlusionCache = new BlockOcclusionCache();
         this.useAmbientOcclusion = Minecraft.useAmbientOcclusion();
-        this.useForgeExperimentalLightingPipeline = ForgeConfig.CLIENT.experimentalForgeLightPipelineEnabled.get();
+        this.useForgeExperimentalLightingPipeline = false;
     }
 
     public void renderModel(BlockRenderContext ctx, ChunkBuildBuffers buffers) {
