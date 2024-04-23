e34d96af89732a85335e2ecd804f192e1e909207
embeddedt <42941056+embeddedt@users.noreply.github.com>
2024-04-23 11:09:08 -0400

    1.20.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import net.fabricmc.loom.task.RemapJarTask
 import net.fabricmc.loom.task.RemapSourcesJarTask
 
 object Constants {
-    const val IS_SNAPSHOT: Boolean = true
+    const val IS_SNAPSHOT: Boolean = false
 }
 
 plugins {
