c11273fc0a333edd4295dda79b7c0bd2f9ef1a97
embeddedt <42941056+embeddedt@users.noreply.github.com>
2024-04-27 16:17:31 -0400

    Add back option list getter in OptionGroupConstructionEvent

--- src/main/java/org/embeddedt/embeddium/api/OptionGroupConstructionEvent.java
+++ src/main/java/org/embeddedt/embeddium/api/OptionGroupConstructionEvent.java
@@ -6,14 +6,10 @@ import org.embeddedt.embeddium.api.eventbus.EventHandlerRegistrar;
 import org.embeddedt.embeddium.client.gui.options.OptionIdentifier;
 
 import java.util.List;
-import java.util.Objects;
 
 /**
  * Fired when an option group is created, to allow replacing options in that group if desired. (Can be used,
  * for instance, to extend the VSync or fullscreen options.)
- *
- * Adding new options to a group is not allowed, you must use {@link OptionPageConstructionEvent} and create
- * a new group.
  */
 public class OptionGroupConstructionEvent extends EmbeddiumEvent {
     public static final EventHandlerRegistrar<OptionGroupConstructionEvent> BUS = new EventHandlerRegistrar<>();
@@ -24,6 +20,10 @@ public class OptionGroupConstructionEvent extends EmbeddiumEvent {
     public OptionGroupConstructionEvent(OptionIdentifier<Void> id, List<Option<?>> options) {
         this.id = id;
         this.options = options;
+    }
+
+    public List<Option<?>> getOptions() {
+        return this.options;
     }
 
     public OptionIdentifier<Void> getId() {
