69cfc3df5641ef9b6b7b82208c5db9302a256bcd
embeddedt <42941056+embeddedt@users.noreply.github.com>
2024-04-27 13:07:27 -0400

    Split vertex type

--- src/main/java/me/jellysquid/mods/sodium/client/render/chunk/vertex/format/impl/VanillaChunkVertex.java
+++ src/main/java/me/jellysquid/mods/sodium/client/render/chunk/vertex/format/impl/VanillaChunkVertex.java
@@ -7,21 +7,22 @@ import me.jellysquid.mods.sodium.client.render.chunk.vertex.format.ChunkMeshAttr
 import me.jellysquid.mods.sodium.client.render.chunk.vertex.format.ChunkVertexEncoder;
 import me.jellysquid.mods.sodium.client.render.chunk.vertex.format.ChunkVertexType;
 import net.caffeinemc.mods.sodium.api.util.ColorABGR;
-import net.caffeinemc.mods.sodium.api.util.ColorU8;
+import net.caffeinemc.mods.sodium.api.util.ColorMixer;
 import org.lwjgl.system.MemoryUtil;
 
 /**
- * This vertex format is less performant and uses more VRAM than {@link CompactChunkVertex}, but should be completely
- * compatible with mods & resource packs that need high precision for models.
+ * This vertex format uses the same format as {@link com.mojang.blaze3d.vertex.DefaultVertexFormat#BLOCK}. It is not
+ * intended to be used with Embeddium's shaders.
  */
-public class VanillaLikeChunkVertex implements ChunkVertexType {
-    public static final int STRIDE = 28;
+public class VanillaChunkVertex implements ChunkVertexType {
+    public static final int STRIDE = 32;
 
     public static final GlVertexFormat<ChunkMeshAttribute> VERTEX_FORMAT = GlVertexFormat.builder(ChunkMeshAttribute.class, STRIDE)
             .addElement(ChunkMeshAttribute.POSITION_MATERIAL_MESH, 0, GlVertexAttributeFormat.FLOAT, 3, false, false)
             .addElement(ChunkMeshAttribute.COLOR_SHADE, 12, GlVertexAttributeFormat.UNSIGNED_BYTE, 4, true, false)
             .addElement(ChunkMeshAttribute.BLOCK_TEXTURE, 16, GlVertexAttributeFormat.FLOAT, 2, false, false)
-            .addElement(ChunkMeshAttribute.LIGHT_TEXTURE, 24, GlVertexAttributeFormat.UNSIGNED_INT, 1, false, true)
+            .addElement(ChunkMeshAttribute.LIGHT_TEXTURE, 24, GlVertexAttributeFormat.UNSIGNED_SHORT, 2, false, true)
+            //.addElement(ChunkMeshAttribute.NORMAL, 28, GlVertexAttributeFormat.UNSIGNED_INT, 1, false, true)
             .build();
 
     @Override
@@ -50,10 +51,15 @@ public class VanillaLikeChunkVertex implements ChunkVertexType {
             MemoryUtil.memPutFloat(ptr + 0, vertex.x);
             MemoryUtil.memPutFloat(ptr + 4, vertex.y);
             MemoryUtil.memPutFloat(ptr + 8, vertex.z);
-            MemoryUtil.memPutInt(ptr + 12, vertex.color);
+
+            // Unpack brightness and apply it to the other three color values to match vanilla format
+            int c = vertex.color;
+            MemoryUtil.memPutInt(ptr + 12, ColorABGR.withAlpha(ColorMixer.mulSingle(c, ColorABGR.unpackAlpha(c)), 255));
             MemoryUtil.memPutFloat(ptr + 16, encodeTexture(vertex.u));
             MemoryUtil.memPutFloat(ptr + 20, encodeTexture(vertex.v));
-            MemoryUtil.memPutInt(ptr + 24, (encodeDrawParameters(material, sectionIndex) << 0) | (encodeLight(vertex.light) << 16));
+            // TODO
+            MemoryUtil.memPutInt(ptr + 24, vertex.light);
+            MemoryUtil.memPutInt(ptr + 28, encodeDrawParameters(material, sectionIndex));
 
             return ptr + STRIDE;
         };
@@ -61,12 +67,6 @@ public class VanillaLikeChunkVertex implements ChunkVertexType {
 
     private static int encodeDrawParameters(Material material, int sectionIndex) {
         return (((sectionIndex & 0xFF) << 8) | ((material.bits() & 0xFF) << 0));
-    }
-
-    private static int encodeLight(int light) {
-        int block = light & 0xFF;
-        int sky = (light >> 16) & 0xFF;
-        return ((block << 0) | (sky << 8));
     }
 
     private static float encodeTexture(float value) {
