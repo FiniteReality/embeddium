10125659714f34589611c7a14e864167f1cb2718
embeddedt <42941056+embeddedt@users.noreply.github.com>
2024-04-27 22:18:10 -0400

    Simplify NeoForge light pipeline handling using new API

--- src/main/java/org/embeddedt/embeddium/render/chunk/light/ForgeLightPipeline.java
+++ src/main/java/org/embeddedt/embeddium/render/chunk/light/ForgeLightPipeline.java
@@ -1,15 +1,11 @@
 package org.embeddedt.embeddium.render.chunk.light;
 
-import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.blaze3d.vertex.VertexConsumer;
 import me.jellysquid.mods.sodium.client.model.light.LightPipeline;
-import me.jellysquid.mods.sodium.client.model.light.data.ArrayLightDataCache;
 import me.jellysquid.mods.sodium.client.model.light.data.LightDataAccess;
 import me.jellysquid.mods.sodium.client.model.light.data.QuadLightData;
 import me.jellysquid.mods.sodium.client.model.quad.ModelQuadView;
 import net.minecraft.client.color.block.BlockColors;
 import net.minecraft.client.renderer.block.model.BakedQuad;
-import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.level.BlockAndTintGetter;
@@ -18,8 +14,6 @@ import net.neoforged.neoforge.client.model.lighting.FlatQuadLighter;
 import net.neoforged.neoforge.client.model.lighting.QuadLighter;
 import net.neoforged.neoforge.client.model.lighting.SmoothQuadLighter;
 import net.neoforged.neoforge.client.textures.UnitTextureAtlasSprite;
-import org.joml.Matrix3f;
-import org.joml.Matrix4f;
 
 /**
  * Implements an Embeddium-compatible frontend for the Forge light pipeline.
@@ -27,10 +21,8 @@ import org.joml.Matrix4f;
 public class ForgeLightPipeline implements LightPipeline {
     private final QuadLighter forgeLighter;
     private final BlockAndTintGetter level;
-    private final LightDataConsumer consumer = new LightDataConsumer();
     private final int[] mutableQuadVertexData = new int[32];
     private final BakedQuad mutableQuad = new BakedQuad(mutableQuadVertexData, -1, Direction.UP, UnitTextureAtlasSprite.INSTANCE, false);
-    private static final PoseStack.Pose EMPTY = new PoseStack.Pose(new Matrix4f(), new Matrix3f());
 
     private long cachedPos = Long.MIN_VALUE;
 
@@ -77,71 +69,13 @@ public class ForgeLightPipeline implements LightPipeline {
         } else {
             forgeQuad = generateForgeQuad(quad);
         }
-        forgeLighter.process(consumer, EMPTY, forgeQuad, OverlayTexture.NO_OVERLAY);
-        System.arraycopy(consumer.lm, 0, out.lm, 0, 4);
-        System.arraycopy(consumer.brightness, 0, out.br, 0, 4);
+        forgeLighter.computeLightingForQuad(forgeQuad);
+        System.arraycopy(forgeLighter.getComputedLightmap(), 0, out.lm, 0, 4);
+        System.arraycopy(forgeLighter.getComputedBrightness(), 0, out.br, 0, 4);
     }
 
     @Override
     public void reset() {
         this.cachedPos = Long.MIN_VALUE;
-    }
-
-    static class LightDataConsumer implements VertexConsumer {
-        float[] brightness;
-        int[] lm;
-
-        // This should always override the exact overload called in QuadLighter#process. We use it to capture the
-        // brightness and lightmap.
-        @Override
-        public void putBulkData(PoseStack.Pose pose, BakedQuad quad, float[] brightness, float r, float g, float b, float a, int[] lm, int overlay, boolean colorize) {
-            this.brightness = brightness;
-            this.lm = lm;
-        }
-
-        @Override
-        public VertexConsumer vertex(double pX, double pY, double pZ) {
-            return this;
-        }
-
-        @Override
-        public VertexConsumer color(int pRed, int pGreen, int pBlue, int pAlpha) {
-            return this;
-        }
-
-        @Override
-        public VertexConsumer uv(float pU, float pV) {
-            return this;
-        }
-
-        @Override
-        public VertexConsumer overlayCoords(int pU, int pV) {
-            return this;
-        }
-
-        @Override
-        public VertexConsumer uv2(int pU, int pV) {
-            return this;
-        }
-
-        @Override
-        public VertexConsumer normal(float pX, float pY, float pZ) {
-            return this;
-        }
-
-        @Override
-        public void endVertex() {
-
-        }
-
-        @Override
-        public void defaultColor(int pDefaultR, int pDefaultG, int pDefaultB, int pDefaultA) {
-
-        }
-
-        @Override
-        public void unsetDefaultColor() {
-
-        }
     }
 }
