2ebbcbc71bd78d4e86f00e9a9dacacdd8f8383e4
embeddedt <42941056+embeddedt@users.noreply.github.com>
2024-04-29 14:40:29 -0400

    Update to 1.20.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("idea")
-    id("net.neoforged.gradle.userdev") version("7.0.112")
+    id("net.neoforged.gradle.userdev") version("7.0.116")
     id("maven-publish")
 
     // This dependency is only used to determine the state of the Git working tree so that build artifacts can be
