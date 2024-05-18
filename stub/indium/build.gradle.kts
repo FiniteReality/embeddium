e8b9d8d8c98a722a8b77c738206f50707d49f4d4
embeddedt <42941056+embeddedt@users.noreply.github.com>
2024-05-17 21:38:49 -0400

    Fix compilation with Java 21 on command line

--- stub/indium/build.gradle.kts
+++ stub/indium/build.gradle.kts
@@ -1,3 +1,9 @@
 plugins {
     id("java-library")
 }
+
+tasks.withType<JavaCompile> {
+    sourceCompatibility = JavaVersion.VERSION_17.majorVersion
+    targetCompatibility = JavaVersion.VERSION_17.majorVersion
+    options.encoding = "UTF-8"
+}
