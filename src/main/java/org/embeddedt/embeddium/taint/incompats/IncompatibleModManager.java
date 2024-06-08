50322b9534cb32c22be5e95b2de678aa7a383c72
embeddedt <42941056+embeddedt@users.noreply.github.com>
2024-06-08 17:12:27 -0400

    Warn when Lazurite is installed

--- src/main/java/org/embeddedt/embeddium/taint/incompats/IncompatibleModManager.java
+++ src/main/java/org/embeddedt/embeddium/taint/incompats/IncompatibleModManager.java
@@ -2,10 +2,7 @@ package org.embeddedt.embeddium.taint.incompats;
 
 import com.google.common.collect.ImmutableList;
 import me.jellysquid.mods.sodium.client.SodiumClientMod;
-import net.minecraftforge.fml.ModLoader;
-import net.minecraftforge.fml.ModLoadingContext;
-import net.minecraftforge.fml.ModLoadingStage;
-import net.minecraftforge.fml.ModLoadingWarning;
+import net.minecraftforge.fml.*;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.forgespi.language.IModInfo;
 
@@ -21,11 +18,16 @@ public class IncompatibleModManager {
             .build();
 
     public static void checkMods(FMLClientSetupEvent event) {
+        IModInfo selfInfo = ModLoadingContext.get().getActiveContainer().getModInfo();
+        if(ModList.get().isLoaded("lazurite")) {
+            event.enqueueWork(() -> {
+                ModLoader.get().addWarning(new ModLoadingWarning(selfInfo, ModLoadingStage.SIDED_SETUP, "Embeddium includes FFAPI support as of 0.3.20, Lazurite should be removed"));
+            });
+        }
         // TODO: Enable
         if (true || SodiumClientMod.options().advanced.disableIncompatibleModWarnings) {
             return;
         }
-        IModInfo selfInfo = ModLoadingContext.get().getActiveContainer().getModInfo();
         String[] modDeclarationList = INCOMPATIBLE_MODS.stream().filter(ModDeclaration::matches).map(ModDeclaration::toString).toArray(String[]::new);
         if(modDeclarationList.length > 0) {
             event.enqueueWork(() -> {
