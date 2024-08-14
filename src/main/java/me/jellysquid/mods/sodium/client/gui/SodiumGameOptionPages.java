4f8235cb8dc3041010d05b6cd516fceece94cbed
embeddedt <42941056+embeddedt@users.noreply.github.com>
2024-08-14 18:29:26 -0400

    Move fullscreen res logic to another class to restore lambda offsets
    Related: #367

--- src/main/java/me/jellysquid/mods/sodium/client/gui/SodiumGameOptionPages.java
+++ src/main/java/me/jellysquid/mods/sodium/client/gui/SodiumGameOptionPages.java
@@ -2,9 +2,7 @@ package me.jellysquid.mods.sodium.client.gui;
 
 import com.google.common.collect.ImmutableList;
 import com.mojang.blaze3d.pipeline.RenderTarget;
-import com.mojang.blaze3d.platform.Monitor;
 import com.mojang.blaze3d.platform.Window;
-import me.jellysquid.mods.sodium.client.SodiumClientMod;
 import me.jellysquid.mods.sodium.client.compat.modernui.MuiGuiScaleHook;
 import me.jellysquid.mods.sodium.client.compatibility.workarounds.Workarounds;
 import me.jellysquid.mods.sodium.client.gl.arena.staging.MappedStagingBuffer;
@@ -21,58 +19,19 @@ import me.jellysquid.mods.sodium.client.gui.options.storage.SodiumOptionsStorage
 import me.jellysquid.mods.sodium.client.render.chunk.compile.executor.ChunkBuilder;
 import net.minecraft.client.*;
 import net.minecraft.network.chat.Component;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.common.ForgeConfig;
-import net.minecraftforge.fml.ModList;
 import org.embeddedt.embeddium.client.gui.options.StandardOptions;
+import org.embeddedt.embeddium.impl.gui.options.FullscreenResolutionHelper;
 import org.embeddedt.embeddium.render.ShaderModBridge;
 import org.lwjgl.opengl.GL;
 import org.lwjgl.opengl.GLCapabilities;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
 
 public class SodiumGameOptionPages {
     private static final SodiumOptionsStorage sodiumOpts = new SodiumOptionsStorage();
     private static final MinecraftOptionsStorage vanillaOpts = new MinecraftOptionsStorage();
-
-    private static boolean isFullscreenResAlreadyAdded() {
-        return ModList.get().isLoaded("embeddium_extra") || ModList.get().isLoaded("rubidium_extra");
-    }
-
-    private static OptionImpl<?, ?> createFullScreenResolutionOption() {
-        Window window = Minecraft.getInstance().getWindow();
-        Monitor monitor = window.findBestMonitor();
-        int maxMode;
-        if (monitor != null) {
-            maxMode = monitor.getModeCount() - 1;
-        } else {
-            maxMode = -1;
-        }
-        ControlValueFormatter formatter = value -> {
-            if (monitor == null) {
-                return Component.translatable("options.fullscreen.unavailable");
-            } else if (value == -1) {
-                return Component.translatable("options.fullscreen.current");
-            } else {
-                return Component.literal(monitor.getMode(value).toString());
-            }
-        };
-        return OptionImpl.createBuilder(int.class, vanillaOpts)
-            .setId(StandardOptions.Option.FULLSCREEN_RESOLUTION)
-            .setName(Component.translatable("options.fullscreen.resolution"))
-            .setTooltip(Component.translatable("embeddium.options.fullscreen.resolution.tooltip"))
-            .setControl(option -> new SliderControl(option, -1, maxMode, 1, formatter))
-            .setBinding((opts, value) -> {
-                if (monitor != null) {
-                    window.setPreferredFullscreenVideoMode(value == -1 ? Optional.empty() : Optional.of(monitor.getMode(value)));
-                    window.changeFullscreenVideoMode();
-                }
-            }, (opts) -> monitor != null ? window.getPreferredFullscreenVideoMode().map(monitor::getVideoModeIndex).orElse(-1) : -1)
-            .build();
-    }
 
     private static int computeMaxRangeForRenderDistance(@SuppressWarnings("SameParameterValue") int injectedRenderDistance) {
         if(vanillaOpts.getData().renderDistance().values() instanceof OptionInstance.IntRange range) {
@@ -146,7 +105,7 @@ public class SodiumGameOptionPages {
                             }
                         }, (opts) -> opts.fullscreen().get())
                         .build())
-                .addConditionally(!isFullscreenResAlreadyAdded(), SodiumGameOptionPages::createFullScreenResolutionOption)
+                .addConditionally(!FullscreenResolutionHelper.isFullscreenResAlreadyAdded(), FullscreenResolutionHelper::createFullScreenResolutionOption)
                 .add(OptionImpl.createBuilder(boolean.class, vanillaOpts)
                         .setId(StandardOptions.Option.VSYNC)
                         .setName(Component.translatable("options.vsync"))
