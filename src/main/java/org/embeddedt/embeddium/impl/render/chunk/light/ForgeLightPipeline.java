c5535166fb90eb7e2b8756cce0a77666a740abf1
embeddedt <42941056+embeddedt@users.noreply.github.com>
2024-08-16 18:45:16 -0400

    Simplify Forge light pipeline handling further

--- src/main/java/org/embeddedt/embeddium/impl/render/chunk/light/ForgeLightPipeline.java
+++ src/main/java/org/embeddedt/embeddium/impl/render/chunk/light/ForgeLightPipeline.java
@@ -5,7 +5,6 @@ import org.embeddedt.embeddium.impl.model.light.data.LightDataAccess;
 import org.embeddedt.embeddium.impl.model.light.data.QuadLightData;
 import org.embeddedt.embeddium.impl.model.quad.ModelQuadView;
 import net.minecraft.client.color.block.BlockColors;
-import net.minecraft.client.renderer.block.model.BakedQuad;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.level.BlockAndTintGetter;
@@ -13,7 +12,6 @@ import net.neoforged.neoforge.client.model.IQuadTransformer;
 import net.neoforged.neoforge.client.model.lighting.FlatQuadLighter;
 import net.neoforged.neoforge.client.model.lighting.QuadLighter;
 import net.neoforged.neoforge.client.model.lighting.SmoothQuadLighter;
-import net.neoforged.neoforge.client.textures.UnitTextureAtlasSprite;
 
 /**
  * Implements an Embeddium-compatible frontend for the Forge light pipeline.
@@ -22,8 +20,6 @@ public class ForgeLightPipeline implements LightPipeline {
     private final QuadLighter forgeLighter;
     private final BlockAndTintGetter level;
     private final int[] mutableQuadVertexData = new int[32];
-    private final BakedQuad mutableQuadWithoutShade = new BakedQuad(mutableQuadVertexData, -1, Direction.UP, UnitTextureAtlasSprite.INSTANCE, false);
-    private final BakedQuad mutableQuadWithShade = new BakedQuad(mutableQuadVertexData, -1, Direction.UP, UnitTextureAtlasSprite.INSTANCE, true);
 
     private long cachedPos = Long.MIN_VALUE;
 
@@ -48,7 +44,7 @@ public class ForgeLightPipeline implements LightPipeline {
         }
     }
 
-    private BakedQuad generateForgeQuad(ModelQuadView quad, boolean hasShade) {
+    private void populateVertexData(ModelQuadView quad) {
         int[] vData = this.mutableQuadVertexData;
         for(int i = 0; i < 4; i++) {
             int vertexBase = i * IQuadTransformer.STRIDE;
@@ -59,19 +55,13 @@ public class ForgeLightPipeline implements LightPipeline {
             // Do not tell Forge about the packed light, so that it doesn't use it in the lightmap calculation
             vData[vertexBase + IQuadTransformer.UV2] = 0;
         }
-        return hasShade ? this.mutableQuadWithShade : this.mutableQuadWithoutShade;
     }
 
     @Override
     public void calculate(ModelQuadView quad, BlockPos pos, QuadLightData out, Direction cullFace, Direction lightFace, boolean shade) {
         this.computeLightData(pos);
-        BakedQuad forgeQuad;
-        if(quad instanceof BakedQuad) {
-            forgeQuad = generateForgeQuad(quad, ((BakedQuad)quad).isShade());
-        } else {
-            forgeQuad = generateForgeQuad(quad, false);
-        }
-        forgeLighter.computeLightingForQuad(forgeQuad);
+        populateVertexData(quad);
+        forgeLighter.computeLightingForQuad(this.mutableQuadVertexData, shade);
         System.arraycopy(forgeLighter.getComputedLightmap(), 0, out.lm, 0, 4);
         System.arraycopy(forgeLighter.getComputedBrightness(), 0, out.br, 0, 4);
     }
