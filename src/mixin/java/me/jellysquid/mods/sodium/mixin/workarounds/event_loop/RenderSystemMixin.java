7d9fa7e81f5569eb2df94d9158a164528543a032
embeddedt <42941056+embeddedt@users.noreply.github.com>
2024-09-07 16:21:21 -0400

    Fix mixins

--- src/mixin/java/me/jellysquid/mods/sodium/mixin/workarounds/event_loop/RenderSystemMixin.java
+++ src/mixin/java/me/jellysquid/mods/sodium/mixin/workarounds/event_loop/RenderSystemMixin.java
@@ -7,7 +7,7 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(RenderSystem.class)
 public class RenderSystemMixin {
-    @Redirect(method = "flipFrame", at = @At(value = "INVOKE", target = "Lorg/lwjgl/glfw/GLFW;glfwPollEvents()V", ordinal = 0), remap = false)
+    @Redirect(method = "flipFrame", at = @At(value = "INVOKE", target = "Lorg/lwjgl/glfw/GLFW;glfwPollEvents()V", ordinal = 0))
     private static void removeFirstPoll() {
         // noop
         // should fix some bugs with minecraft polling events twice for some reason (why does it do that in the first place?)
