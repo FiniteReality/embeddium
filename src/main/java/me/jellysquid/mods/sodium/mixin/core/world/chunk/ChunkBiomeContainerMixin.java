faa03082fb9254a92d642d4107883ea5a7865842
embeddedt <42941056+embeddedt@users.noreply.github.com>
2024-09-19 10:20:54 -0400

    Tweak cloning strategy for compatibility with mods modifying biome container

--- src/main/java/me/jellysquid/mods/sodium/mixin/core/world/chunk/ChunkBiomeContainerMixin.java
+++ src/main/java/me/jellysquid/mods/sodium/mixin/core/world/chunk/ChunkBiomeContainerMixin.java
@@ -9,17 +9,17 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(ChunkBiomeContainer.class)
-public class ChunkBiomeContainerMixin implements ChunkBiomeContainerExtended {
+public abstract class ChunkBiomeContainerMixin implements ChunkBiomeContainerExtended {
     @Shadow
     @Final
     private IdMap<Biome> biomeRegistry;
 
     @Shadow
-    @Final
-    private Biome[] biomes;
+    public abstract int[] writeBiomes();
 
     @Override
     public ChunkBiomeContainer embeddium$copy() {
-        return new ChunkBiomeContainer(this.biomeRegistry, this.biomes.clone());
+        int[] biomeIds = this.writeBiomes();
+        return new ChunkBiomeContainer(this.biomeRegistry, biomeIds);
     }
 }
